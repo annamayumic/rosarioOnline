d144c44db17bc47f014aa361b9f4ed8fe35b8f93
annamayumic <93163995+annamayumic@users.noreply.github.com>
2025-11-13 12:10:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -54,13 +54,12 @@ var introSalveRainha = new Partes('','Infinitas graças vos damos, Soberana Rain
 var SalveRainha = new Partes('Salve Rainha', 'Salve, Rainha, mãe de misericórdia, vida, doçura, esperança nossa, salve! A Vós bradamos, os degredados filhos de Eva. A Vós suspiramos, gemendo e chorando neste vale de lágrimas. Eia, pois, advogada nossa, esses Vossos olhos misericordiosos a nós volvei. E, depois deste desterro, nos mostrai Jesus, bendito fruto do Vosso ventre. Ó clemente, ó piedosa,ó doce Virgem Maria. Rogai por nós, Santa Mãe de Deus, para que sejamos dignos das promessas de Cristo. Amém.', false)
 
 //-----listas de objetos ----//
-var lista = [cruz, introCredo, credo, paiNosso, introAveMarias, gloria, ohJesus]
+var lista = [cruz, introCredo, credo, paiNosso, introAveMariaUm, aveMaria, introAveMariaDois, aveMaria, introAveMariaTres, aveMaria, gloria, ohJesus]
 var combo = [paiNosso, aveMaria,aveMaria,aveMaria,aveMaria,aveMaria,aveMaria,aveMaria,aveMaria,aveMaria,aveMaria, gloria, ohJesus]
 var gozosos = [mGo1,  mGo2,  mGo3, mGo4, mGo5]
 var dolorosos = [mDo1, mDo2, , mDo3,mDo4, mDo5]
 var gloriosos = [mGlo1,  mGlo2, mGlo3, mGlo4, mGlo5]
 var luminosos = [Lu1, Lu2, Lu3, Lu4, Lu5]
-var introAveMarias = [introAveMariaUm, aveMaria, introAveMariaDois, aveMaria, introAveMariaTres, aveMaria]
 
 //----- construcao da lista principal---//
 function rosarioInteiro(){
@@ -147,7 +146,7 @@ function executar(a){
   function Clique(){
     display.innerHTML = `<h1>${a[contador].title}</h1> <p>${a[contador].content}</p>` ;
     
-    if(contador==6||contador ==8||contador==10){
+    if(contador==4||contador ==6||contador==8){
       if(a[contador-2]=== aveMaria){
         document.querySelectorAll('div .tresmarias')[c-1].style.backgroundColor = ' rgb(237, 218, 166)'
       }
@@ -176,6 +175,7 @@ function executar(a){
     
     contador++;
 }}
+
 
 
 
