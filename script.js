e35799de6f2366098c25c1d204697d4c0383086e
annamayumic <93163995+annamayumic@users.noreply.github.com>
2025-11-17 01:02:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -146,8 +146,8 @@ function executar(a){
   function Clique(){
     display.innerHTML = `<h1>${a[contador].title}</h1> <p>${a[contador].content}</p>` ;
     
-    if(contador>3 &contador<10){
-      if(a[contador-2]=== aveMaria2){
+    if(contador>3 && contador<10){
+      if(a[contador-2]==aveMaria2){
         document.querySelectorAll('div .tresmarias')[c-2].style.backgroundColor = ' rgb(237, 218, 166)'
       }
       tremarias.style.display="flex";
@@ -177,6 +177,7 @@ function executar(a){
     contador++;
 }
 }
+
 
 
 
